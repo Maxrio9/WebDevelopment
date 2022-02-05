6b65408e628835d0b326ec5697ce8b43c2189bc5
Maxrio <lasse@rosendahl-energietechnik.de>
2022-02-05 14:38:01 +0100

    Added introduction, people and actions

---
+++ Mixed Messages Portfolio Project/Mixed_Messages.js
@@ -0,0 +1,26 @@
+// When run will output random message
+
+const introductions = [
+    'My sources have told me that',
+    'The villagers are saying that',
+    'Some anarchist that walked along said that',
+    'Your own wife said that',
+    'The mayor said that'
+]
+const people = [
+    'the mayor',
+    'your wife',
+    'your son',
+    'your brother',
+    'your grandmother',
+    'Elon Musk'
+]
+
+const actions = [
+    'is a liar.',
+    'talks to him/herself.',
+    'plans to colonize mars.',
+    'is a lovely person',
+    'likes turtles.'
+]
+
